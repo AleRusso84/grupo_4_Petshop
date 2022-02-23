1df84e000d416e1e2d95ef0b7c3a80f9bbf3876c
AleRusso84 <95726838+AleRusso84@users.noreply.github.com>
2022-02-22 21:26:37 -0300

    ¨agregado recuerdame en login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,39 @@
 const express = require('express'); //requerimos express
 const router = express.Router();
-//const path = require('path');
-//const validations = require('../middleware/validationUser');
+const path = require('path');
+const multer=require('multer')
+const validations = require('../middleware/validationUser');
 const userController = require ('../controllers/userController');
 const guestMiddleware = require ('../middleware/guestMiddleware');
 const authMiddleware = require ('../middleware/authMiddleware');
 
-//const uploadFile = require('../middleware/multerPerfil');
+const uploadFile = require('../middleware/multerPerfil');
+
+let storage=multer.diskStorage({
+    destination:(req,file,callback)=>{
+        let folder=path.join(__dirname,'../../public/images/imagesPerfil');
+        callback(null,folder);
+    },
+    filename:(req, file, callback)=>{
+        let imageName= "user-"+Date.now()+path.extname(file.originalname);
+        callback(null, imageName);
+    }
+})
+
+let upload=multer({storage:storage});
 
 
 router.get('/register',userController.registro)
 
-router.get('/login',userController.login)
+//Procesar el registro
+//router.post('/register', uploadFile.single('imagesPerfil'), validations,userController);
 
-router.get("/list",userController.list)
+router.get('/login',userController.login);
+
+//Procesar el login
+//router.post('/login',userController);
+
+router.get("/list",userController.list);
+router.post("/list",userController.list);
 
 module.exports=router
