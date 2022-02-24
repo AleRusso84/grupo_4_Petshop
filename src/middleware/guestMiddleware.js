3cd5b7bbdde38852f201a79769b6cf70bb2f4d55
AleRusso84 <95726838+AleRusso84@users.noreply.github.com>
2022-02-24 19:24:23 -0300

    arreglo middle

--- src/middleware/guestMiddleware.js
+++ src/middleware/guestMiddleware.js
@@ -1,8 +1,19 @@
 function guestMiddleware (req, res, next){
-    if (req.session.userLogged){
-        return res.redirect ('/users/detail')
+    if (req.session.user) {
+        // Lo dejamos pasar
+        
+        res.redirect('/users/profile');
+    } else {
+        next();
     }
-    next();
+
+
+
+
+  //  if (req.session.userLogged){
+  //      return res.redirect ('/users/detail')
+  //  }
+  //  next();
 }
 
 module.exports = guestMiddleware;
