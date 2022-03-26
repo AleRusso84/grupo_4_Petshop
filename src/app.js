27d9c8ce7d660a1d749f33578db903cef0829962
Marcos Fernandez <marcosezq@gmail.com>
2022-03-26 13:40:03 -0300

    Arreglo de los productos multiplicados y agrefo el css y java del menu

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.set('views', path.join(__dirname,'./views'))
 
 app.use("/",homeRouter)
 app.use("/",productsRouter )
-app.use('/users', userRoutes);
+app.use("/", userRoutes);
 
 // ************ catch 404 and forward to error handler ************
 app.use((req, res, next) => next(createError(404)));
