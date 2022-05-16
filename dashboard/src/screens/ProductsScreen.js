3e3e8ea2179a0b89b8b28f6c4a98c6fab190f910
AleRusso84 <95726838+AleRusso84@users.noreply.github.com>
2022-05-16 15:53:52 -0300

    screens

--- dashboard/src/screens/ProductsScreen.js
+++ dashboard/src/screens/ProductsScreen.js
@@ -88,4 +88,5 @@ class ProductsScreen extends Component {
   }
 }
 
+
 export default ProductsScreen;
--- dashboard/src/screens/UsersScreen.js
+++ dashboard/src/screens/UsersScreen.js
@@ -77,4 +77,5 @@ class UsersScreen extends Component {
   }
 }
 
+
 export default UsersScreen;
